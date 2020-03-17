e16fb9c1628126a037c7bfb4383a0d9181182880
joneii <itsgener@gmail.com>
2020-03-16 22:52:45 -0400

    js was separated into multiple files for easier reading. leaderboard added. To Do list on css page

---
+++ js/questions.js
@@ -0,0 +1,28 @@
+//Variable declarations for questions
+var questions = [
+    {
+      title: "Question 1:",
+      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+      answer: "answer from choices"
+    },
+    {
+      title: "Question 2:",
+      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+      answer: "answer from choices"
+    },
+    {
+      title: "Question 3:",
+      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+      answer: "answer from choices"
+    },
+    {
+      title: "Question 4:",
+      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+      answer: "answer from choices"
+    },
+    {
+      title: "Question 5:",
+      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+      answer: "answer from choices"
+    }
+  ];
--- js/timer.js
+++ js/timer.js
@@ -1,66 +1,101 @@
 //Variables selecting the timer location and Begin Quiz button
-var timer = document.getElementById("timer");
 var beginButton = document.getElementById("beginQuiz");
-
-//Variable declarations for questions
-var questions = [
-    {
-      title: "Question 1:",
-      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-      answer: "answer from choices"
-    },
-    {
-      title: "Question 2:",
-      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-      answer: "answer from choices"
-    }
-  ]
-  [  
-  {
-      title: "Question 3:",
-      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-      answer: "answer from choices"
-    }
-  ]
-  [
-    {
-      title: "Question 4:",
-      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-      answer: "answer from choices"
-    }
-  ]
-  [
-    {
-      title: "Question 5:",
-      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-      answer: "answer from choices"
-    }
-  ];
+var instructionsHTML = document.getElementById("instructions");
+var questionsHTML = document.getElementById("question");
+var timer = document.getElementById("timer");
+var time = 90;
+var interval;
+var quizFinish = false;
 
 
-//Function for starting that quiz
+
+//Function for starting the quiz
 function startQuiz(){
     
+    displayTime();
+    hideInstructions();
+    showQuestions();
 
-    var time = 90;
-    timer.innerText ="Time Left: " + time;
-
-
-    setInterval(function(){
-        time = time - 1;
-        timer.innerText ="Time Left: " + time;
-    }, 1000);
-
-    var instructions = document.getElementById("instructions");
-    instructions.parentNode.removeChild(instructions);
-
-
-
+    interval = setInterval(function(){
+                countDown();
+            }, 1000);
     
 }
 
+//Function continues counting down the time until the quiz ends
+function countDown(){
+    //Ends the quiz when timer reaches 0 or all questions have
+    //been answered
+    if(time == 0 || quizFinish){
+        endQuiz();
+    }
+    else{
+        time = time - 1;
+        displayTime();
+    }
+}
+
+//Displays the current time
+function displayTime(){
+    timer.innerText ="Time Left: " + time;
+}
+
+//Stops the timer
+function clearTimer(){
+    clearInterval(interval);
+}
+
+//Clears the instructions from the screen and begins the quiz
+function hideInstructions(){
+    instructionsHTML.style.display = "none";
+}
+
+//Begins quiz by bringing in the questions
+function showQuestions(){
+    questionsHTML.style.display = "block";
+}
+
+function hideQuestions(){
+    questionsHTML.style.display = "none";
+}
+
+//Function that ends the quiz, ask user for initials and post
+//to leader board.
+function endQuiz(){
+    clearTimer();
+    hideQuestions();
+    
+    if(quizFinish){
+        showResults();
+    }
+    else{
+        showTimeOut();
+    }
+}
+
+function showresults(){
+    //To-DO
+}
+
+function showTimeOut(){
+    //To-Do
+}
 
 //Event listener for the Begin Quiz button
 beginButton.addEventListener("click", startQuiz);
 
 
+
+//TO - DO list
+//- html for questions
+//- html for timeout scenario
+//- html for finish all questions scenario
+//- html for clear leaderboard button
+//- css style on everything
+//- html get rid leaderboard link
+//- js for leaderboard
+//- js to show quesiton
+//- js for incorrect answer scenario
+//- js for correct answer scenario
+//- js for score management
+//- js to bring instructions back after quiz finishes
